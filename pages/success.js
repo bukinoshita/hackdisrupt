b70c6bb6ec9bf5c2380ff5408d98e61e2ef81d68
Bu Kinoshita <bukinoshita@gmail.com>
2017-11-01 08:38:28 -0400

    on `success` page, should request a single user (https://github.com/bukinoshita/hackdisrupt/issues/50)

--- pages/success.js
+++ pages/success.js
@@ -19,26 +19,16 @@ import ButtonLink from './../ui/button-link'
 class Success extends Component {
   static async getInitialProps({ query }) {
     const id = query.id
-    const res = await fetch(`${process.env.API_URL}/users`, {
+    const res = await fetch(`${process.env.API_URL}/user/${id}`, {
       headers: {
         Authorization: process.env.API_TOKEN
       }
     })
     const json = await res.json()
-    const user = json.users.map(user => {
-      if (user.githubId === id) {
-        return user
-      }
-
-      return {
-        name:
-          'Esse `id` não existe, mas você pode entrar na nossa lista beta sua coisa linda!'
-      }
-    })[0]
 
     setToken(id)
 
-    return { user }
+    return { user: json.user }
   }
 
   componentDidMount() {
